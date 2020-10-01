688a1493adb955a17b3a1a61b5303e23323bf2f5
coltrida <coltrida@gmail.com>
2020-10-01 21:52:33 +0200

    CRUD for Category

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\QuestionController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('/question', QuestionController::class);
+Route::apiResource('/category', CategoryController::class);
